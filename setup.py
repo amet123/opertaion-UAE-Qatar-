1fca452f27525c0030f67951d94763aa7b574e2d
amet123 <ametvaid@gmail.com>
2026-04-14 11:08:49 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+    install_requires = [l.strip() for l in f if l.strip() and not l.startswith("#")]
 
 setup(
     name="operations_module",
@@ -13,4 +13,5 @@ setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
+    python_requires=">=3.10",
 )
